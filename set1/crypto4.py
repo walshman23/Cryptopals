efdd1b899f2be84b8713b53e46b57a7750c8b81a
Walshman23 <walshman23@users.noreply.github.com>
2014-08-21 20:34:53 -0500

    Added solution to set 1, challenge 4

---
+++ set1/crypto4.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+
+# // ------------------------------------------------------------
+# 4. Detect single-character XOR
+# 
+# One of the 60-character strings at:
+# 
+#  https://gist.github.com/3132713
+# 
+# has been encrypted by single-character XOR. Find it. (Your code from
+# #3 should help.)
+
+
+import string
+import binascii
+import ltrfreq
+import utils
+
+scores = []
+results = []
+lineno = 0
+
+
+
+with open("gistfile1.txt") as f:
+	for line in f:
+		lineno = lineno + 1;		
+		hexstring = string.rstrip(line)
+		b = binascii.unhexlify(hexstring) 	
+		for key in range(256):
+			candidatebuf = utils.xor(key, b)
+			scores.append(ltrfreq.totcscore(candidatebuf) + ltrfreq.norvigscore(candidatebuf)) 
+			spiffy = utils.SpiffyPrint(candidatebuf)
+			results.append(spiffy)
+				
+
+print "Highest score is", max(scores), " Sekrit message is", results[scores.index(max(scores))]
+print "Sekrit message came from line", scores.index(max(scores))/256 + 1
+
+# Now that the party is jumping. //  7b5a4215415d544115415d5015455447414c155c46155f4058455c5b523f
