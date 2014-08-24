757005b03c5f5902691cb59b1ef236ae62a22fd0
Walshman23 <walshman23@users.noreply.github.com>
2014-08-23 21:30:16 -0500

    Working on determining KEYSIZE

--- set1/crypto6.py
+++ set1/crypto6.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-#  // ------------------------------------------------------------
+# // ------------------------------------------------------------
 #  
 #  6. Break repeating-key XOR
 #  
@@ -59,12 +59,42 @@ import ltrfreq
 s1 = "this is a test"
 s2 = "wokka wokka!!!"
 
-
 hamster = utils.hamming(s1, s2)
 print hamster
 
 assert hamster == 37
 
 
-# That's all for now
+# That's all for now - below here doesnt work
+
+decoded = []
+bytes1 = []
+bytes2 = []
+
+minkeylength = 2
+maxkeylength = 41
+
+
+with open("set1/gistfile3132752.txt") as f:
+    for line in f:
+        decoded += binascii.a2b_base64(line)
+
+buflength = len(decoded)
+
+
+
+
+for keysize in range(minkeylength, maxkeylength):
+    numbufs = buflength/keysize
+    print "Input contains", buflength, "bytes, allowing for", numbufs,"buffers of", keysize,"bytes to be read"
+    for i in range(1,numbufs/2):
+        #read pair of bufs
+        tbuf1 = decoded[(i-1)*keysize:i*keysize]
+        tbuf2 = decoded[(i)*keysize:(i+1)*keysize]
+        #score pair of bufs
+
+        #add score to accumulator
+        #end
+    #Score for this keysize is (accumulator/bufpairs)/keysize
+
 
