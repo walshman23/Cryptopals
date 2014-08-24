fb156ea70b3775ba437067981f94f60c753c0722
Walshman23 <walshman23@users.noreply.github.com>
2014-08-24 10:28:13 -0500

    Jerking around with identifying keysize.  Still not working/complete.

--- set1/crypto6.py
+++ set1/crypto6.py
@@ -76,30 +76,34 @@ bytes1 = []
 bytes2 = []
 
 minkeylength = 2
-maxkeylength = 41
+maxkeylength = 40
 
 
 with open("./gistfile3132752.txt") as f:
-    for line in f:
-        decoded += binascii.a2b_base64(line)
+    rawf = f.read()
+    decoded += binascii.a2b_base64(rawf)
 
 buflength = len(decoded)
 
 
 
 
-for keysize in range(minkeylength, maxkeylength):
+for keysize in range(minkeylength, maxkeylength + 1):
     numbufs = buflength/keysize
-    print "Input contains", buflength, "bytes, allowing for", numbufs,"buffers of", keysize,"bytes to be read"
-    for i in range(1,numbufs/2):
+    bufpairs = numbufs/2
+  #  print "Input contains", buflength, "bytes, allowing for", numbufs,"buffers of", keysize,"bytes to be read"
+    accumulator = 0.0;
+    for bufpairno in range(1,bufpairs,2):
         #read pair of bufs
-        tbuf1 = decoded[(i-1)*keysize:i*keysize]
-        tbuf2 = decoded[(i)*keysize:(i+1)*keysize]
+        #  print "buf1",(bufpairno-1)*keysize, bufpairno*keysize - 1
+        #  print "buf2",bufpairno*keysize, (bufpairno+1)*keysize - 1
+        tbuf1 = decoded[(bufpairno-1)*keysize:bufpairno*keysize - 1] #Check this
+        tbuf2 = decoded[(bufpairno)*keysize:(bufpairno+1)*keysize - 1] #This too
 
         #score pair of bufs
+        accumulator = accumulator + utils.hamming(tbuf1,tbuf2)
 
-        #add score to accumulator
-        #end
     #Score for this keysize is (accumulator/bufpairs)/keysize
+    print "Score for keylength", keysize, "is", (accumulator/bufpairs)/keysize
 
-
+# Keysize of 2 seems lowest. Hmmm. I smell a bug.
